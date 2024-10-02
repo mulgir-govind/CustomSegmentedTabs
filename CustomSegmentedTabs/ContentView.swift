0081a1b3d3c536c93ef4a203783c62c1fbc516e1
govind-mulgir <govind.mulgir@pubmatic.com>
2024-10-02 18:34:54 +0530

    Initial Commit

---
+++ CustomSegmentedTabs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomSegmentedTabs
+//
+//  Created by Govind on 02/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomSegmentedTabs/CustomSegmentedTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSegmentedTabsApp.swift
+//  CustomSegmentedTabs
+//
+//  Created by Govind on 02/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomSegmentedTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
